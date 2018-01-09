2a9c3e89c32a4b34a9b94a4991c9f86c97a9c710
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-09 20:38:40 +0000

    Added name and books properties to Library class including tests

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Library {
+    private String name;
+    private ArrayList<Book> books;
+
+    public Library(String name){
+        this.name = name;
+        this.books = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getBookCount() {
+        return this.books.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void before(){
+        library = new Library("Ellon Library");
+        book1 = new Book();
+        book2 = new Book();
+    }
+
+    @Test
+    public void libraryHasName(){
+        assertEquals("Ellon Library", library.getName());
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.getBookCount());
+    }
+
 }
